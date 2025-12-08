2d6c824c1a1fdde2653f4904c62d6f700fc772af
JhonJerickGuyala <gjhonjherick@gmail.com>
2025-12-09 00:08:51 +0800

    Week 2- Day 2: Customer Am Routes - Modify All Routes

--- routers/customer/CustomerAmRoutes.js
+++ routers/customer/CustomerAmRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { getAllAmenities, getAmenityById } from '../../controller/customer/CustomerAmController.js';
+// 👇 FIX 1: Remove { } brackets. Import the whole object.
+import CustomerAmController from '../../controller/customer/CustomerAmController.js';
 
 const router = express.Router();
 
-router.get('/', getAllAmenities);
-router.get('/:id', getAmenityById);
+// 👇 FIX 2: Use the correct method names defined in the controller (getAll, getById, getFeatured)
+
+// Order matters! 'featured' must come BEFORE '/:id'
+router.get('/featured', CustomerAmController.getFeatured); 
+
+router.get('/', CustomerAmController.getAll);
+router.get('/:id', CustomerAmController.getById);
 
 export default router;
