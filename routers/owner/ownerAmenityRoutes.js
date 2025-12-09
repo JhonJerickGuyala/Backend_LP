f088829b81997121f31305eb49ae73a2f756ebc7
JhonJerickGuyala <gjhonjherick@gmail.com>
2025-12-09 10:28:13 +0800

    Week 2 -Day 3: Owner Amenity Routes – Modify All Routes

--- routers/owner/ownerAmenityRoutes.js
+++ routers/owner/ownerAmenityRoutes.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import OwnerAmenityController from '../../controller/owner/OwnerAmenityController.js';
-import upload from '../../middleware/upload.js'; 
+import { uploadAmenities } from '../../middleware/upload.js'; 
+
+import { protect, authorize } from '../../middleware/authMiddleware.js'; 
 
 const router = express.Router();
 
-router.get('/', OwnerAmenityController.getAll);
-router.post('/', upload.single('image'), OwnerAmenityController.create);
-router.put('/:id', upload.single('image'), OwnerAmenityController.update);
-router.delete('/:id', OwnerAmenityController.delete);
+router.get('/', protect, authorize('owner'), OwnerAmenityController.getAll);
+
+router.post('/', 
+    protect, 
+    authorize('owner'), 
+    uploadAmenities.single('image'), 
+    OwnerAmenityController.create
+);
+
+router.put('/:id', 
+    protect, 
+    authorize('owner'), 
+    uploadAmenities.single('image'), 
+    OwnerAmenityController.update
+);
+
+router.delete('/:id', protect, authorize('owner'), OwnerAmenityController.delete);
 
 export default router;
