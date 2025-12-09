6aace4b0762c83502ae9c7ce5c961999d1746451
JhonJerickGuyala <gjhonjherick@gmail.com>
2025-12-09 10:35:42 +0800

    Week 2 - Day 5: Reservation Routes – Modify All Routes

--- routers/ReservationRoutes.js
+++ routers/ReservationRoutes.js
@@ -1,18 +1,15 @@
 import express from 'express';
 import ReservationController from '../controller/ReservationController.js';
 
+import { protect } from '../middleware/authMiddleware.js'; 
+
 const router = express.Router();
 
-// Get all reservations (admin)
-router.get('/', ReservationController.getAll);
-
-// Get reservation by ID
-router.get('/:id', ReservationController.getById);
-
-// Update reservation
-router.put('/:id', ReservationController.update);
-
-// Delete reservation
-router.delete('/:id', ReservationController.delete);
+router.get('/', protect, ReservationController.getAll);
+router.get('/:id', protect, ReservationController.getById);
+router.get('/user/:userId', protect, ReservationController.getByUserId);
+router.put('/:id', protect, ReservationController.update);
+router.put('/:id/extend', protect, ReservationController.extend);
+router.delete('/:id', protect, ReservationController.delete);
 
 export default router;
